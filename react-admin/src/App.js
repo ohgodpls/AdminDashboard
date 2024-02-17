deed1564797cc828bfab1fae97f35a0266cbc985
Sham Srinivasan <s.sham95@gmail.com>
2024-02-16 21:59:55 -0800

    added top bar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+//import Sidebar from "./scenes/global/Sidebar";
+//import Dashboard from "./scenes/dashboard";
+//import Team from "./scenes/team";
+//import Invoices from "./scenes/invoices";
+//import Contacts from "./scenes/contacts";
+//import Bar from "./scenes/bar";
+//import Form from "./scenes/form";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import FAQ from "./scenes/faq";
+//import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+//import Calendar from "./scenes/calendar/calendar";
+
 function App() {
+  const [theme, colorMode] = useMode();
+//  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- react-admin/src/theme.js
+++ react-admin/src/theme.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useMemo } from "react";
-import { createTheme } from "mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 // Color design tokens
 export const tokens = (mode) => ({
