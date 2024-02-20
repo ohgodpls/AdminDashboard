e91e0c9bea5140729f069ce0357f677700dac8a7
Sham Srinivasan <s.sham95@gmail.com>
2024-02-20 00:09:46 -0800

    added side bar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
-//import Sidebar from "./scenes/global/Sidebar";
-//import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
 //import Team from "./scenes/team";
 //import Invoices from "./scenes/invoices";
 //import Contacts from "./scenes/contacts";
@@ -12,9 +12,14 @@ import Topbar from "./scenes/global/Topbar";
 //import Pie from "./scenes/pie";
 //import FAQ from "./scenes/faq";
 //import Geography from "./scenes/geography";
+//import Electrical from "./scenes/electical";
+//import Mechanical from "./scenes/mechanical";
+//import Instrumentation from "./scenes/instrumentation";
+//import Quality from "./scenes/qualityins";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 //import Calendar from "./scenes/calendar/calendar";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -25,8 +30,26 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+             {/* <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/mechanical" element={<Mechanical />} />
+              <Route path="/electrical" element={<Electrical />} />
+              <Route path="/instrumentation" element={<Instrumentation />} />
+              <Route path="/qualityins" element={<Quality />} />*/}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
